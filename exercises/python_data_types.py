1485abfe797ead1e0e79de1c4ace47538237a490
ThalesFerreira <thalesferreira190@gmail.com>
2023-02-06 04:38:36 -0300

    Aula do dia 20/1/2023

---
+++ exercises/python_data_types.py
@@ -0,0 +1,58 @@
+def is_bool(value):
+    return type(bool)
+
+
+def is_int(value):
+    return type(int)
+
+
+def is_float(value):
+    return type(float)
+
+
+def is_string(value):
+    return type(str)
+
+
+def is_list(value):
+    return type(list)
+
+
+def is_tuple(value):
+    return type(tuple)
+
+
+def is_set(value):
+    return type(set)
+
+
+def is_dict(value):
+    return type(dict)
+
+
+def return_lower_case_string(word):
+    return
+
+
+def append_element_in_list(element, input_list):
+    return
+
+
+def remove_element_from_list(element, input_list):
+    return
+
+
+def create_new_key_value_in_dict(key, value, input_dict):
+    return
+
+
+def delete_key_from_dict(key, input_dict):
+    return
+
+
+def add_element_to_set(element, input_set):
+    return
+
+
+def remove_element_from_set(element, input_set):
+    return
